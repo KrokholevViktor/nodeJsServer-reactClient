eef9eb00f4e88e1c589bedc1feb36856813428f6
Krokholev Viktor <xxxre.paprika@gmail.com>
2024-03-30 22:23:51 +0700

    jwt token

---
+++ server/index.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+
+const express = require('express')
+const sequelize = require('./db')
+const models = require('./models/model') 
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const router = require('./routes/index') //импорт главного роутера
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
+
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+//настройка корс
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static'))) //раздавать файлы с папки static
+app.use(fileUpload({})) //для загрузки изображений
+app.use('/api', router)
+
+
+
+//Обработка ошибок, последний Middleware
+app.use(errorHandler)
+
+//функция для подключения к базе данных
+const start = async () => {
+    try {
+        await sequelize.authenticate() //подключение к базе данных
+        await sequelize.sync() //сверяет состояние БД со схемой
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (error) {
+        console.log(e);
+    }
+}
+
+start()
