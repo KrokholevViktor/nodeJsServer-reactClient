270a69d20d37158494f6fdd7c3c5dceeb2179143
Krokholev Viktor <xxxre.paprika@gmail.com>
2024-04-01 01:40:57 +0700

    client v0.2 01.04.24

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,23 +1,29 @@
 import React, { useContext } from "react";
 import { Context } from "..";
-import {Navbar, Nav, Container} from 'react-bootstrap'
+import {Navbar, Nav, Container, Button} from 'react-bootstrap'
 import { Link } from "react-router-dom";
 import { SHOP_ROUTE } from "../utils/consts";
+import {observer} from 'mobx-react-lite';
 
-const NavBar = () => {
+const NavBar = observer(() => {
     const {user} = useContext(Context)
     return (
         <Navbar bg="dark" data-bs-theme="dark">
             <Container>
-                <Link to={SHOP_ROUTE}>Купи девайс</Link>
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
+                <Link style={{color: 'white', textDecoration: 'none'}} to={SHOP_ROUTE}>КупиДевайс</Link>
+                {user.isAuth ?
+                    <Nav className="ml-auto">
+                        <Button variant={'outline-light'}>Админ панель</Button>
+                        <Button variant={'outline-light'} className="ms-2" onClick={() => user.setIsAuth(false)}>Выйти</Button>
+                    </Nav>
+                    :
+                    <Nav className="ml-auto">
+                        <Button variant={'outline-light'} onClick={() => user.setIsAuth(true)}>Авторизация</Button>
+                    </Nav>
+                }
             </Container>
         </Navbar>
     )
-}
+})
 
 export default NavBar;
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { Container, Form, Card, Button } from "react-bootstrap";
 
 const Auth = () => {
     return (
-        <div>
-            Auth
-        </div>
+            <Container 
+            className="d-flex justify-content-center align-items-center"
+            style={{height: window.innerHeight - 54}}
+            >
+                <Card style={{width: 600}} className="p-5">
+                    <h2 className="m-auto">Авторизация</h2>
+                    <Form className="d-flex flex-column">
+                        <Form.Control
+                            className="mt-3"
+                            placeholder="Введите ваш email..."
+                        />
+                        <Form.Control
+                            className="mt-3"
+                            placeholder="Введите пароль"
+                        />
+                        <Button 
+                        variant="outline-primary" 
+                        className="mt-3 align-self-end">
+                            Войти
+                        </Button>
+                    </Form>
+                </Card>
+            </Container>
     )
 }
  
